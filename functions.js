ca0e6fa95d0398d8311f7a8aec31e64a5f4c3587
Mikayda Mills <mills.ma12@gmail.com>
2019-01-23 22:05:06 -0500

    added bean counter code

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 // your control flow code from chapter 2 of eloquent js goes here!
 
-/*MINIMUM EXERCISE*/
+//MINIMUM EXERCISE
 
 function min(arg1, arg2) {
   if (arg1 < arg2) {
@@ -8,4 +8,26 @@ function min(arg1, arg2) {
   } else {
     return arg2;
   }
+}
+
+//BEAN COUNTING
+
+function countBs(string) {
+  let num = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "B") {
+      num++;
+    }
+  }
+  return num;
+}
+
+function countChar(string, char) {
+  let num = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === char) {
+      num++;
+    }
+  }
+  return num;
 }
