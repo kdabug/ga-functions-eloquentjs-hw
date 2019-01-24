bb16bbce128fb614777d083380211154bf2c47a6
Mikayda Mills <mills.ma12@gmail.com>
2019-01-23 21:16:52 -0500

    added code for minimum exercise

--- functions.js
+++ functions.js
@@ -1,1 +1,11 @@
 // your control flow code from chapter 2 of eloquent js goes here!
+
+/*MINIMUM EXERCISE*/
+
+function min(arg1, arg2) {
+  if (arg1 < arg2) {
+    return arg1;
+  } else {
+    return arg2;
+  }
+}
